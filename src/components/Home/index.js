720794bba30397997f04406321c467ab2a32da99
root <pniruth@gmail.com>
2023-11-15 05:52:54 +0000

    solution

---
+++ src/components/Home/index.js
@@ -0,0 +1,39 @@
+// Write your code here
+import {Component} form 'react'
+
+import Login from '../Login'
+import Logout from '../Logout'
+import Message from '../Message'
+
+import './index.css'
+
+class Home extends Component{
+    state={isLoggedIn : false}
+
+    onClickButton=()=> {
+        this.setState(prevState=>({isLoggedIn:!prevState.isLoggedIn}))
+    }
+
+    render(){
+        const {isLoggedIn}=this.state
+
+        return(
+            <div className="container">
+             <div className="login-card-container">
+                <Message isLoggedIn={isLoggedIn}/>
+
+                {isLoggedIn ?( <Logout logout={this.onClickButton}/>)
+                : ( <Login login={this.onClickButton}/>
+
+                )}
+
+             </div>
+
+            </div>
+
+        )
+    }
+}
+
+export default Home
+
